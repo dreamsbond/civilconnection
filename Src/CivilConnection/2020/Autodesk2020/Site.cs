22342701275bfd26fdd8223e1c9a66f617d6f086
Jasper Ngan <j.ngan@fugro.com>
2025-02-11 11:48:50 +0800

    Add Site.cs.

---
+++ Src/CivilConnection/2020/Autodesk2020/Site.cs
@@ -0,0 +1,71 @@
+﻿using Autodesk.AECC.Interop.Land;
+using Autodesk.DesignScript.Geometry;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CivilConnection
+{
+    /// <summary>
+    /// Site object type.
+    /// </summary>
+    public class Site
+    {
+        #region PRIVATE PROPERTIES
+        /// <summary>
+        /// The name
+        /// </summary>
+        string _name;
+        /// <summary>
+        /// The Site
+        /// </summary>
+        private AeccSite _site;
+        #endregion
+
+        #region PUBLIC PROPERTIES
+        /// <summary>
+        /// Gets the name.
+        /// </summary>
+        /// <value>
+        /// The name.
+        /// </value>
+        public string Name { get { return this._name; } set { this._name = value; } }
+        #endregion
+
+        #region CONSTRUCTOR
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Site"/> class.
+        /// </summary>
+        /// <param name="s">The AeccSite.</param>
+        internal Site(AeccSite s)
+        {
+            this._site = s;
+            this._name = s.Name;
+        }
+        #endregion
+
+        #region PUBLIC METHODS
+        /// <summary>
+        /// Gets the land feature lines.
+        /// </summary>
+        /// <returns></returns>
+        public IList<LandFeatureline> GetLandFeatureLines()
+        {
+            Utils.Log(string.Format("CivilDocument.getLandFeatureLines started...", ""));
+
+            IList<LandFeatureline> output = new List<LandFeatureline>();
+
+            foreach (AeccLandFeatureLine fl in this._site.FeatureLines)
+            {
+                output.Add(new LandFeatureline(fl));
+            }
+
+            Utils.Log(string.Format("CivilDocument.getLandFeatureLines completed.", ""));
+
+            return output;
+        }
+        #endregion
+    }
+}
